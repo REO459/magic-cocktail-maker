20b9c43c52175c4fed12fe1a718be9e14371dc37
REO459 <bossmob597@gmail.com>
2023-03-23 11:13:57 -0500

    This is my magic cocktail maker.

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla inventore laudantium quos, aliquam dolorem autem ut modi beatae! Quibusdam numquam quia labore, optio nihil suscipit molestias accusantium aliquid, vel reiciendis excepturi laboriosam ipsa. Exercitationem ratione sunt totam beatae laborum quos.
+      </p>
+    </section>
+  )
+}
+
+export default About
